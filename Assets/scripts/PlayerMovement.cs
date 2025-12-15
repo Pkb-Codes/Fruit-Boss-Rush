2b3793fca1e0973dd2ab801ee4f1551f28e5091d
Pkb-Codes <krishnabhargavpuli@gmail.com>
2025-12-15 12:18:20 +0530

    player movement added
    added player movement and a test level, the movement needs some more fixing

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 10f;
+    public float jumpSpeed = 10f;
+    private float move;
+    private Rigidbody2D body;
+
+    bool isGrounded = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input reading for horizontal movement
+        if(Input.GetKey(KeyCode.A)) {
+            move = -1f;
+        }else if(Input.GetKey(KeyCode.D)) {
+            move = 1f;
+        }else {
+            move = 0f;
+        }
+        
+        //horizontal movement
+        body.linearVelocity = new Vector2(move * moveSpeed, body.linearVelocityY);
+
+        //vertical movement
+        if(Input.GetKeyDown(KeyCode.W) && isGrounded) {
+            body.linearVelocity = new Vector2(body.linearVelocityX, jumpSpeed);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //checking if player touches the ground
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
